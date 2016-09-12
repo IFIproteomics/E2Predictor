25165d7b3d1289fc52617f4439e5c4bd72d71057
mesontau <mesontau@gmail.com>
2016-09-12 11:26:28 +0200

    cosmetics

--- inst/scripts/merge.omics.R
+++ inst/scripts/merge.omics.R
@@ -5,16 +5,18 @@ initConfiguration(netMHCpath = "/Users/napedro/external_sources/netMHC-4.0/netMH
 
 cell.lines <- names(E2Predictor.Config$hla_alleles)
 conditions <- c("Control", "Bortezomib", "DMOG", "IFNg", "Rapamycin")
-cell.line <- cell.lines[4]
+cell.line <- cell.lines[1]
 
 dbfile <- file.path(E2Predictor.Config$working.path, "databases", "canonical", "uniprot-human.tab.gz")
 is_perturbation <- T
 condition <- conditions[1]
-addgrepcond <- "_II_"
+addgrepcond <- " I "
 minReplicates <- 1
 # You don't need to have the allele predictions, it makes the predictions when allele.predictions = NULL
 #allele.predictions <- readRDS(file.path(E2Predictor.Config$working.path, "allele.predictions", ifelse(is_perturbation, "perturbations", "cell_lines"), cell.line, "allele.predictions.Rds"))
 allele.predictions <- NULL
+allele.predictions = readRDS(file.path(E2Predictor.Config$working.path, "allele.predictions", "perturbations", "JY", "Control_I","allele.predictions.Rds"))
+
 allele.predictor <- "ic50"
 
 nM.threshold <- 1000
